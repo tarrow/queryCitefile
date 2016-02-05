fc625d53b57f62cda325984142c79900d16c19d7
Thomas Arrow <thomasarrow@gmail.com>
2016-02-05 10:12:24 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from  import setup
+
+setup(name='queryCitefile',
+      install_requires=[],
+      version='0.1',
+      description='Simple tool to process mwcites output',
+      author='Thomas Arrow',
+      author_email='thomasarrow@gmail.com',
+      url='https://github.com/tarrow/queryCiteFile',
+      modules=['queryCiteFile']
+      )
